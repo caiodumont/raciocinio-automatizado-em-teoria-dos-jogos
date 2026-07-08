45b7f1a5e4ad26d2c419ce3e91bbe5d459f76bc9
Caio Dumont <caiodumontfer@gmail.com>
2026-07-07 22:16:28 -0300

    fix: otimizar representação das tabelas de utilidade
    Substitui listas de tuplas por dicionários para otimizar consultas e atualizações de payoffs, além de revisar nomenclatura, comentários e convenções de código para melhorar a legibilidade da implementação.

--- src/nash_sat.py
+++ src/nash_sat.py
@@ -1,28 +1,30 @@
 import itertools
-import networkx
+import networkx as nx
 import random
 
-def gerar_jogos_aleatorios(n: int, s: int, d: int) -> list:
-    # Gera um jogo gráfico aleatório com n jogadores, s estratégias e d arestas.
-    # Retorna as utilidades de cada jogador e suas respectivas vizinhanças.
-    utilidades = []
+def gerar_jogo_grafico_aleatorio(n_jogadores: int, s_estrategias: int, d_arestas: int) -> tuple:
+    # Gera um jogo gráfico aleatório.
+    # Retorna a tabela de utilidade de cada jogador considerando os perfis de estratégias da sua vizinhança, 
+    # além das respectivas vizinhanças.
+    tabelas_utilidade = []
     vizinhancas = []
 
-    # Gera um grafo aleatório com n vértices (jogadores) e d arestas
-    grafo = networkx.gnm_random_graph(n, d)
+    # Gera o grafo de interação entre os jogadores
+    grafo_interacao = nx.gnm_random_graph(n_jogadores, d_arestas)
 
-    for i in range(n):
-        jogador = []
+    for jogador in range(n_jogadores):
+        tabela_utilidade = {}
 
-        vizinhanca = sorted([i, *grafo.neighbors(i)])
+        # Obtém a vizinhança do jogador, incluindo o próprio jogador
+        vizinhanca = sorted([jogador, *grafo_interacao.neighbors(jogador)])
 
-        # Percorre todas as combinações de estratégias possíveis para os jogadores da vizinhança
-        for combinacao in itertools.product(range(1, s+1), repeat=len(vizinhanca)):
-            payoff = random.randint(1, s**len(vizinhanca))
-            jogador.append((combinacao, payoff))
+        # Percorre todos os perfis de estratégias possíveis da vizinhança
+        for perfil in itertools.product(range(1, s_estrategias+1), repeat=len(vizinhanca)):
+            payoff = random.randint(1, s_estrategias**len(vizinhanca))
+            tabela_utilidade[perfil] = payoff
 
-        # Armazena a tabela de utilidade do jogador e sua respectiva vizinhança
-        utilidades.append(jogador)
+        # Armazena a tabela de utilidade e a vizinhança do jogador atual
+        tabelas_utilidade.append(tabela_utilidade)
         vizinhancas.append(vizinhanca)
 
-    return utilidades, vizinhancas
+    return tabelas_utilidade, vizinhancas
